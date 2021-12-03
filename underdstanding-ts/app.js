7539e486f387ae947f8529a0be84e7082990b0eb
Woo-yeong Kim <kimwy1997@gmail.com>
2021-12-03 23:17:54 +0900

    Core types

---
+++ underdstanding-ts/app.js
@@ -0,0 +1,11 @@
+var person = {
+    name: "Wooyeong",
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
+};
+var favActivities;
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
