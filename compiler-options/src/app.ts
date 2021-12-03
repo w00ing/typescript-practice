1c3a4f8f5b8a61b1d844db6b28387eaf2b711b99
Woo-yeong Kim <kimwy1997@gmail.com>
2021-12-04 03:35:57 +0900

    Complier Options

---
+++ compiler-options/src/app.ts
@@ -0,0 +1,9 @@
+let age: number;
+age = 30;
+const userName = "Wooyeong";
+console.log(userName);
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", () => {
+  console.log("Clicked");
+});
