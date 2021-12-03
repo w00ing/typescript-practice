b6bd78c6654a7769d9e0088251991cbd2ef65791
Woo-yeong Kim <kimwy1997@gmail.com>
2021-12-03 23:29:57 +0900

    Unions and Aliases

--- underdstanding-ts/app.js
+++ underdstanding-ts/app.js
@@ -1,11 +1,22 @@
-var person = {
-    name: "Wooyeong",
-    age: 30,
-    hobbies: ["Sports", "Cooking"]
-};
-var favActivities;
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    //   if (resultConversion === "as-number") {
+    //     return +result;
+    //   } else {
+    //     return result.toString();
+    //   }
 }
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Woo", "Yeong", "as-text");
+console.log(combinedNames);
